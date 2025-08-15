6e203e85b450abf14e63295cc2064aa749645a4c
Matheus-Janczkowski <matheusj2009@hotmail.com>
2025-08-15 11:41:05 -0300

    updates the setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Grante',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy'
+    ],
+    include_package_data=True,
+    package_data={
+        # Include additional files like images, text files, etc.
+        # '': ['*.txt', '*.rst'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        # Add more classifiers as needed
+    ],
+    python_requires='>=3.6',  # Specify the required Python version
+)
