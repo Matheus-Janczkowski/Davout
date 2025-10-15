552832bd5f0d21f8f816c52c16be03791ab9fba3
Matheus-Janczkowski <matheusj2009@hotmail.com>
2025-10-15 13:46:13 -0300

    updates plotting tools not to depend upon the copy of error bars

--- source/Grante/PythonicUtilities/aa_tests/tests_plotting_tools.py
+++ source/Grante/PythonicUtilities/aa_tests/tests_plotting_tools.py
@@ -82,7 +82,7 @@ class TestPlots(unittest.TestCase):
         plot_object = plotting_tools.plane_plot(x_data=
         self.unimodal_x_data, y_data=self.unimodal_y_data, file_name=
         "test_error_bar_separate_plotting", plot_type="scatter", 
-        error_bar=deepcopy(error_bar), color="black")
+        error_bar=error_bar, color="black")
 
         plotting_tools.plane_plot(x_data=self.unimodal_x_data, y_data=
         self.unimodal_y_data2, file_name="test_error_bar_separate_plot"+
@@ -91,7 +91,7 @@ class TestPlots(unittest.TestCase):
         # Scatter single curve given the error bar
 
         plotting_tools.plane_plot(x_data=self.unimodal_x_data, y_data=
-        self.unimodal_y_data, error_bar=deepcopy(error_bar), file_name=
+        self.unimodal_y_data, error_bar=error_bar, file_name=
         "test_error_bar", plot_type="scatter")
 
         # Scatter single curve given the error bar with upper and lower
@@ -104,7 +104,7 @@ class TestPlots(unittest.TestCase):
         "wer bounds")
 
         plotting_tools.plane_plot(x_data=self.unimodal_x_data, y_data=
-        self.unimodal_y_data, error_bar=deepcopy(error_bar_lower_upper), 
+        self.unimodal_y_data, error_bar=error_bar_lower_upper, 
         file_name="test_error_bar_lower_upper", plot_type="scatter")
 
         # Continuous single curve given the error bar
@@ -112,7 +112,7 @@ class TestPlots(unittest.TestCase):
         print("\nTests error plot for a continuous curve")
 
         plotting_tools.plane_plot(x_data=self.unimodal_x_data, y_data=
-        self.unimodal_y_data, error_bar=deepcopy(error_bar), file_name=
+        self.unimodal_y_data, error_bar=error_bar, file_name=
         "test_error_region", plot_type="line")
 
         # Calls with multimodal data
@@ -129,7 +129,7 @@ class TestPlots(unittest.TestCase):
         print("\nTests error plot for multiple continuous curve")
 
         plotting_tools.plane_plot(x_data=self.multimodal_x_data, y_data=
-        self.multimodal_y_data, error_bar=deepcopy(error_bar), file_name=
+        self.multimodal_y_data, error_bar=error_bar, file_name=
         "test_error_region_multimodal", plot_type="line")
 
         # Continuous single curve automatically evaluating the error bar
