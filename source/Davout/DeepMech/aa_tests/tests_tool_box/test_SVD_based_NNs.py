afaab9530eb3b2f8b40528509dae160e58fe7845
Matheus-Janczkowski <matheusj2009@hotmail.com>
2026-07-08 20:22:49 -0300

    more validated features

--- source/Davout/DeepMech/aa_tests/tests_tool_box/test_SVD_based_NNs.py
+++ source/Davout/DeepMech/aa_tests/tests_tool_box/test_SVD_based_NNs.py
@@ -4,9 +4,7 @@ import time
 
 import tensorflow as tf
 
-from ...tool_box import ANN_tools
-
-from ...tool_box import training_tools
+from ...tool_box import ANN_tools, training_tools, parameters_tools, differentiation_tools
 
 from ....PythonicUtilities.testing_tools import run_class_of_tests
 
@@ -22,19 +20,27 @@ class TestSVDArchitecture:
 
         self.quotient_space_dimension = 3
 
+        self.number_of_neurons_hidden_layer_main_network = 4
+
         self.output_dimension = 2
 
         self.activation_list_main_network = [{"quadratic": {"number of"+
-        " neurons": 100, "a2": 1.0}}, {"linear": self.output_dimension}]
+        " neurons": self.number_of_neurons_hidden_layer_main_network, 
+        "a2": 1.0}}, {"linear": self.output_dimension}]
 
         self.accessory_activation_list_tests = [{"quadratic": {"number"+
-        " of neurons": 3, "a2": 1.0}}, {"linear":self.output_dimension}]
+        " of neurons": min(self.quotient_space_dimension,
+        self.number_of_neurons_hidden_layer_main_network), "a2": 1.0}}, 
+        {"linear": min(self.output_dimension, 
+        self.number_of_neurons_hidden_layer_main_network)}]
 
         # Sets the parameters of the custom SVD-based architecture
 
+        self.modulating_function = "smooth_absolute_value"
+
         self.custom_architecture = {
         "name": "SVDQuotientSpace", "weights modulating function": 
-        "identity", "Householder epsilon": 1.0, 
+        self.modulating_function, "Householder epsilon": 1.0, 
         "activations accessory layer list": self.accessory_activation_list_tests}
 
         self.n_samples_tests = 1000
@@ -63,9 +69,9 @@ class TestSVDArchitecture:
 
         # Creates the new test data
 
-        self.x_min = -1.0
+        self.x_min = -10.0
 
-        self.x_max = 1.0
+        self.x_max = 10.0
 
         data_matrix = []
 
@@ -127,6 +133,116 @@ class TestSVDArchitecture:
         self.quotient_space_dimension)
 
         self.custom_model = ANN_class()
+
+    # Defines a function to test evaluating the model from input and 
+    # with trainable parameters to assert if the result is the same in
+    # both cases
+
+    def test_input_and_parameters(self):
+
+        # Gets the model trainable parameters and their shapes
+
+        self.initial_model_parameters, self.parameters_shapes = parameters_tools.model_parameters_to_flat_tensor_and_shapes(
+        self.custom_model)
+
+        # Sets the class to evaluate the model output given the traina-
+        # ble parameters
+
+        self.model_output_given_parameters = parameters_tools.ModelOutputGivenTrainableParameters(
+        self.custom_model, self.parameters_shapes)
+
+        # Gets the training data as a tensorflow array
+
+        self.training_input_constant = tf.constant(self.training_data, 
+        dtype=tf.as_dtype(self.parameters_dtype))
+
+        # Gets the response of the model using the trainable parameters
+        # as an argument
+
+        y_given_parameters = self.model_output_given_parameters(
+        self.training_input_constant, self.initial_model_parameters)
+
+        # Gets the response of the model using the built-in parameters
+
+        y_from_input = self.custom_model(self.training_input_constant)
+
+        print("\nThe response of the model given parameters is:\n"+str(
+        y_given_parameters)+"\n\nwhereas the response of the model whe"+
+        "n evaluated from input is: "+str(y_from_input)+"\n\nThe maxim"+
+        "um absolute difference between components is: "+str(
+        tf.reduce_max(tf.abs(y_from_input-y_given_parameters)).numpy())+
+        "\n")
+
+    # Defines a function to test the derivative of the loss function to 
+    # assert if automatic differentiation is working properly
+
+    def test_derivative(self):
+
+        # Converts the training true values to a tensorflow constant
+
+        self.training_true_values_constant = tf.constant(
+        self.training_true_values, dtype=tf.as_dtype(
+        self.parameters_dtype))
+
+        # Gets an instance of the class that evaluates the loss function
+        # and its derivative from the trainable parameters
+
+        gradient_class = differentiation_tools.ScalarGradientWrtTrainableParamsGivenParameters(
+        self.loss_metric, self.custom_model, 
+        self.training_input_constant, self.parameters_shapes, 
+        self.initial_model_parameters, model_true_values=
+        self.training_true_values_constant, parameters_type=
+        self.initial_model_parameters.dtype)
+
+        # Evaluates the loss function and the derivative with respect to
+        # the trainable parameters
+        
+        loss_value, gradient_wrt_params = gradient_class(
+        self.initial_model_parameters) 
+
+        # Evaluates the loss function with respect to the trainable pa-
+        # rameters directly from the trainable variables stored in the
+        # model
+
+        with tf.GradientTape() as tape:
+
+            loss = self.loss_metric(self.training_true_values_constant,
+            self.custom_model(self.training_input_constant))
+
+        gradient_input = tape.gradient(loss, 
+        self.custom_model.trainable_variables)
+
+        # Prints the gradient for each trainable variable
+
+        gradient_input_string = ""
+
+        for tensor, trainable_variable in zip(gradient_input, 
+        self.custom_model.trainable_variables):
+
+            gradient_input_string += "\n\n"+str(trainable_variable.name
+            )+"\n"+str(tensor)
+
+        print("\nThe gradient evaluated from the input directly is: "+
+        gradient_input_string+"\n")
+
+        # Flattens the gradient to compare with the one calculated from
+        # given parameters
+
+        flat_gradient_input = tf.concat([tf.reshape(gradient, [-1]
+        ) for gradient in gradient_input], axis=0)
+
+        # Stacks the two gradients into a single matrix for comparison
+
+        gradient_matrix = tf.stack([flat_gradient_input, 
+        gradient_wrt_params], axis=0)
+
+        print("The two methods of evaluating the gradient are presente"+
+        "d below. The first\nrow show the gradient evaluated when the "+
+        "model is called from input; the\nsecond row shows the gradien"+
+        "t when the model is called with the given\ntrainable paramete"+
+        "rs as well\n\n"+str(gradient_matrix)+"\n\nThe maximum absolut"+
+        "e difference between components is: "+str(tf.reduce_max(tf.abs(
+        flat_gradient_input-gradient_wrt_params)).numpy())+"\n")
 
     # Defines a function to test training a model with this architecture
 
@@ -231,7 +347,8 @@ if __name__=="__main__":
     # Creates a list of methods (using their names) that are not to be
     # tested
 
-    reserved_methods = []
+    reserved_methods = ["test_monte_carlo_training_model", 
+    "test_quotient_space_invariance_model"]
 
     # Calls the function to run all the necessary tests
 
